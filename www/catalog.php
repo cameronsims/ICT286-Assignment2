75e0b6f3683f3d39ea36dd431953623893d6c24b
Cameron Sims <59544722+cameronsims@users.noreply.github.com>
2024-11-01 22:11:17 +0800

    Delete www directory

--- www/catalog.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// SQL stuff
-include_once("sql.php");
-include_once("catalog-head.php");
-
-catalog_run();
-
-?>
--- www/user.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// SQL stuff
-include_once("sql.php");
-include_once("user-head.php");
-
-user_exec();
-
-?>
